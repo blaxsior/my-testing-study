e40f795c83827b4351c514d53ea85e35dc22fd10
blaxsior <heejun2798@gmail.com>
2023-08-07 15:17:48 +0900

    test: transformToNumber, symbol 타입 변수를 입력하는 경우에 대한 테스트케이스 작성 + 기존 테스트케이스 일부 수정

--- 01/basic-testing-frontend/src/util/numbers.test.js
+++ 01/basic-testing-frontend/src/util/numbers.test.js
@@ -4,7 +4,7 @@ import { transformToNumber } from "./numbers";
 describe("test: transformToNumber unit", () => {
   it("should return number if value is number", () => {
     const value = 10.3;
-    const expected = 10.3;
+    const expected = +value;
 
     const result = transformToNumber(value);
 
@@ -13,25 +13,37 @@ describe("test: transformToNumber unit", () => {
 
   it("should return number if value is numeric string", () => {
     const value = "10.3";
-    const expected = 10.3;
+    const expected = +value;
 
     const result = transformToNumber(value);
-
+    // typeof NaN === 'number' 이므로 toBeTypeOf로는 타입 체크 불가능
+    // expect(result).toBeTypeOf('number');
     expect(result).toBe(expected);
   });
 
-  it("should return NaN if value is neither number nor numeric string", () => {
-    const value = {test: 'hello'};
+  it("should return NaN if value is neither number nor numeric string(non-transformable)", () => {
+    const v1 = {test: 'hello'};
+    const v2 = "invalid";
 
-    const result = transformToNumber(value);
+    const r1 = transformToNumber(v1);
+    const r2 = transformToNumber(v2);
+
+    expect(r1).toBeNaN();
+    expect(r2).toBeNaN();
+  });
+
+  it("should return NaN if no argument passed", () => {
+    // undefined을 전달하는 것과 동일한 효과
+    const result = transformToNumber();
 
     expect(result).toBeNaN();
   });
 
-  it("should return NaN if no argument passed", () => {
-
-    const result = transformToNumber();
-
-    expect(result).toBeNaN();
+  it("should throw error if typeof argument === \'symbol\'", () => {
+    const value = Symbol('invalid');
+    const targetFn = () => {
+      const result = transformToNumber(value);
+    }
+    expect(targetFn).toThrow();
   })
 });
