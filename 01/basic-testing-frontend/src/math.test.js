154bd95027e0add5f27656f77b5d5950ba802d7f
blaxsior <heejun2798@gmail.com>
2023-08-07 12:12:26 +0900

    test: 01-frontend, add 함수 테스트 코드 수정 & 추가
    - 작성된 테스트 코드를 AAA 패턴에 맞게 리팩토링
    - 코드를 간결하게 수정
    - array 내 숫자 덧셈을 검증하는 테스트 코드를 하나만 남김
    - array에 숫자가 아닌 값이 들어오는 경우에 대한 테스트 코드 작성

--- 01/basic-testing-frontend/src/math.test.js
+++ 01/basic-testing-frontend/src/math.test.js
@@ -2,17 +2,25 @@ import { describe, expect, it } from 'vitest';
 import { add } from './math.js';
 describe("test: math.js", () => {
   it("should summarize all values in array", () => {
-    const result = add([1,2,3,4,5,6,7,8,9,10]);
-    expect(result).toBe(55);
-  })
-  it("add 1 + 2 = 3", () => {
-    const result = add([1, 2]);
+    // Arrange
+    const numbers = [1,2]; // 이유 없이 복잡하게 만들지 마
+    const expected = numbers.reduce((a,b) => a + b, 0);
 
-    expect(result).toEqual(3);
+    const result = add(); // Act
+    expect(result).toBe(expected); // Assert
+  });
+  it("should return NaN if at least one of values is not number type", () => {
+    const values = [1, 'b'];
+
+    const result = add(values);
+    expect(result).toBeNaN();
   });
   it("send empty array = 0", () => {
-    const result = add([]);
-    expect(result).toEqual(0);
+    const numbers = [];
+    const expected = 0;
+
+    const result = add(numbers);
+    expect(result).toEqual(expected);
   });
   it("should throw error if no array passed", () => {
     //https://jestjs.io/docs/expect#tothrowerror
