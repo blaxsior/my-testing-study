49e4b09e3cb1fb8f0e3963180078ab33fa4d5b0b
blaxsior <heejun2798@gmail.com>
2023-08-11 12:32:48 +0900

    test: generateToken~ 함수들 테스팅 추가

---
+++ 02/async-code-starting-project/async/async-example.test.js
@@ -0,0 +1,39 @@
+import { describe, it, expect } from 'vitest';
+import { generateToken, generateTokenPromise } from './async-example';
+
+describe("generateToken func", () => {
+  it("should generate token value", (done) => {
+    const testUserEmail = 'test@test.com';
+
+    generateToken(testUserEmail, (err, token) => {
+      try {
+        expect(token).toBeDefined();
+        done();
+      } catch(err) {
+        done(err);
+      }
+    })
+  });
+});
+
+describe("generateTokenPromise func", () => {
+  it("should generate token vlaue", () => {
+    const testUserEmail = 'test@test.com';
+
+    return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+  });
+
+  it("should generate token vlaue", async () => {
+    const testUserEmail = 'test@test.com';
+
+    const token = await generateTokenPromise(testUserEmail);
+    
+    expect(token).toBeDefined();
+  });
+
+  it("should generate token vlaue", async () => {
+    const testUserEmail = 'test@test.com';
+
+    await expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+  });
+});
