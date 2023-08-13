b0c1bd6efdd357c58b6b6540866faec2d604f565
blaxsior <heejun2798@gmail.com>
2023-08-13 21:47:44 +0900

    test: errors, validation 모듈 테스트 추가
    - errors: 데이터가 제공되지 않는 경우 undefined를 보장하는 테스팅 코드 추가
    - validation
      - 텍스트가 white space로 구성되는 경우 커버
      - 에러 메시지 확인 코드 추가

--- 04/more-deeper/util/errors.test.js
+++ 04/more-deeper/util/errors.test.js
@@ -3,12 +3,28 @@ import { HttpError, ValidationError } from './errors.js';
 
 describe('HttpError class', () => {
   it('should have own properties(statusCode, message, data)', () => {
-    const error = new HttpError(200, 'test', 'test');
-
+    //arrange
+    const testStatus = 1;
+    const testMessage = 'test';
+    const testData = { test: 'data' };
+    //act
+    const error = new HttpError(testStatus, testMessage, testData);
+    //assert
     expect(error).toHaveProperty('statusCode');
     expect(error).toHaveProperty('message');
     expect(error).toHaveProperty('data');
   });
+  // 데이터는 제공되지 않아도 괜찮음
+  it('should contain undefined data if no data provided', () => {
+    const testStatus = 1;
+    const testMessage = 'test';
+
+    const error = new HttpError(testStatus, testMessage);
+
+    expect(error.statusCode).toBe(testStatus);
+    expect(error.message).toBe(testMessage);
+    expect(error.data).toBeUndefined();
+  })
 });
 
 describe('ValidationError class', () => {
--- 04/more-deeper/util/validation.test.js
+++ 04/more-deeper/util/validation.test.js
@@ -18,6 +18,15 @@ describe("validateNotEmpty func", () => {
     const targetFn = () => { validateNotEmpty(text, errorMessage) };
     expect(targetFn).toThrow(ValidationError);
   });
+  
+  // white space로 구성되는 경우 필요 없는 문장 -> Error 반환
+  it("should throw ValidationError if text consists of white space", () => {
+    const text = ' \t\n';
+
+    const targetFn = () => { validateNotEmpty(text, errorMessage) };
+    expect(targetFn).toThrow(ValidationError);
+  });
+
 
   it("should throw ValidationError if typeof text is not \'string\'", () => {
     // text 아니면 trim 메서드 / length 프로퍼티 X
@@ -33,4 +42,13 @@ describe("validateNotEmpty func", () => {
     expect(tFn2).toThrow();
     expect(tFn3).toThrow();
   });
+
+  // validation 함수가 던지는 에러의 메시지는 인자로 전달한 메시지와 동일
+  it('should throw error with the provided error message', () => {
+    const text = '';
+
+    const targetFn = () => { validateNotEmpty(text, errorMessage)};
+
+    expect(targetFn).toThrow(errorMessage);
+  })
 });
