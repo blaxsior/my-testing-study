438c3d7176f622597f5bed952865cc85dc9f8bb2
blaxsior <heejun2798@gmail.com>
2023-08-14 14:05:32 +0900

    test: http 모듈 테스트 코드 추가 & 코드 위치 변경
     - 정상적인 데이터가 들어오면 에러가 발생하지 않음을 보장하는 테스트 코드 작성
     - 테스트 코드를 이해하기 쉽도록 mockImplementationOnce을 연관된 테스트 코드 내부로 이동

--- 04/more-deeper/util/http.test.js
+++ 04/more-deeper/util/http.test.js
@@ -2,40 +2,50 @@ import { vi, describe, it, expect, afterEach } from 'vitest';
 import { sendDataRequest } from './http';
 import { HttpError } from './errors';
 
-const fetchMockResult = {test: 'test'};
+const fetchMockResult = { test: 'test' };
 const fetchMock = vi.fn();
-fetchMock.mockImplementationOnce((URL, config) => {
+
+const rejectMessage = 'body is not a string';
+
+fetchMock.mockImplementation((URL, config) => {
   return new Promise((resolve, reject) => {
-    resolve({
+    if (typeof config.body !== 'string') {
+      return reject(rejectMessage);
+    }
+
+    const resolveObj = {
       json: () => new Promise((resolve, reject) => {
         resolve(fetchMockResult);
       }),
       ok: true
-    });
-  });
-});
-fetchMock.mockImplementationOnce((URL, config) => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      json: () => new Promise((resolve, reject) => {
-        resolve(fetchMockResult);
-      }),
-      ok: false
-    });
+    };
+
+    resolve(resolveObj);
   });
 });
 vi.stubGlobal('fetch', fetchMock)
-// fetchMock.mockImplementationOnce((URL, config) => {
-//   return {
-//     json: () => new Promise((resolve, reject) => {
-//       reject('test');
-//     })
-//   }
-// });
 
 describe('sendDataRequest func', () => {
   afterEach(() => {
     fetchMock.mockClear();
+  });
+
+  //mockimplOnce
+  it('should throw HttpError if fail to fetch', async () => {
+    fetchMock.mockImplementationOnce((URL, config) => {
+      return new Promise((resolve, reject) => {
+        const rejectObj = {
+          json: () => new Promise((resolve, reject) => {
+            resolve(fetchMockResult);
+          }),
+          ok: false // 로직에 관련된 부분
+        };
+        resolve(rejectObj);
+      });
+    });
+    const input = '';
+
+    await expect(sendDataRequest(input)).rejects.toThrow(HttpError);
   });
 
   it('should return data if success fetch', async () => {
@@ -47,9 +57,16 @@ describe('sendDataRequest func', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should throw HttpError if fail to fetch', async () => {
-    const input = '';
 
-    await expect(sendDataRequest(input)).rejects.toThrow(HttpError);
-  })
+  it('should return any available response data', async () => {
+    const input = { a: 'b' };
+    let errorMessage = '';
+    try {
+      await sendDataRequest(input);
+    } catch (error) {
+      errorMEssage = error;
+    }
+    expect(errorMessage).not.toBe(rejectMessage);
+    //await expect(sendDataRequest(input)).not.rejects.toThrow(rejectMessage);
+  }); // resolve / reject와 not은 잘 매칭되지 않을 수 있다
 })
